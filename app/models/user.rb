c3c1c2358f33f3fd59daa7ef841811f37ddcc698
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-16 17:44:11 +0400

    Fixing error
    Слово осталось от старого приложения BLOG с комментариями.
    Исправил на labels.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Связь с молделью Label: (User имеет много :Labels) 
-  has_many :comments       
+  has_many :labels       
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
