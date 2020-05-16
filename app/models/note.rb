c1403ee653724bd92e09f6081a4a14f9806d301e
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-16 18:37:57 +0400

    Relationship (ONE User to MANY Notes)
    Создаём связь между таблицами users & notes
    (ОДИН юзер имеет МНОГО заметок)
    (каждая заметка имеет только одного пользователя)

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,4 @@
 class Note < ApplicationRecord
+  # Связь с таблицей молдели User: (Note принадлежит одному :User) 
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Связь с молделью Label: (User имеет много :Labels) 
-  has_many :labels       
+  # Связь с таблицей молдели Label: (User имеет много :Labels) 
+  has_many :labels  
+
+  # Связь с таблицей молдели Notes: (User имеет много :Notes) 
+  has_many :notes      
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
