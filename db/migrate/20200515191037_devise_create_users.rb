a4334098c36b5f543124c8d5aa23191b9a37afb6
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-16 15:16:10 +0400

    Add admin and user_name to User migration
    В файл миграции добавили два дополнительных поля:
          ## OPR addet _____________________
          t.boolean :admin,     default: false
          t.string  :user_name, default: ""
    признак администратора и
    имя пользователя

--- db/migrate/20200515191037_devise_create_users.rb
+++ db/migrate/20200515191037_devise_create_users.rb
@@ -31,7 +31,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+      
+      ## OPR addet
+      t.boolean :admin,     default: false
+      t.string  :user_name, default: ""
 
       t.timestamps null: false
     end
