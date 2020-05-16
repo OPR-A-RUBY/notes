dd383a0ffc16038518e7b4b73c6634fe3672136f
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-16 18:51:21 +0400

    Relationship (ONE Label to MANY Notes)
    Создаём связь между таблицами labels & notes
    (ОДИН лейбл имеет МНОГО заметок)
    (каждая заметка имеет только однин лейбл)

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,7 @@
 class Label < ApplicationRecord
   # Связь с таблицей молдели User: (Label принадлежит одному :User) 
-  belongs_to :user     
- 
+  belongs_to :user
+
+  # Связь с таблицей молдели Notes: (Label имеет много :Notes) 
+  has_many :notes  
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,7 @@
 class Note < ApplicationRecord
   # Связь с таблицей молдели User: (Note принадлежит одному :User) 
   belongs_to :user
+
+  # Связь с таблицей молдели Label: (Note принадлежит одной :Label) 
+  belongs_to :label
 end
