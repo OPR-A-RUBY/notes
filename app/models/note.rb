2a23bb956a475473fd09380788736ac1c76b9379
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-17 17:41:21 +0400

    Add validation for notes (title, body)
    Добавим проверку на наличие Названия и Тела записки.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,8 @@
 class Note < ApplicationRecord
+
+  validates :title, presence: true  # Валидация на непустое поле
+  validates :body, presence: true  # Валидация на непустое поле
+  
   # Связь с таблицей молдели User: (Note принадлежит одному :User) 
   belongs_to :user
 
