8944b418c4c229603ba13f22c0c1613fdf345980
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-22 22:56:16 +0400

    Add single rooute to page 'autor_site'
    Добавим строчку в маршрутный файл, которая указывает где и как обрабатывать ссылку на страницу: 'autor_site'

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'  # Стартовая страница обрабатывается контроллером home 
   get 'home/index'
+  get 'autor-site' => 'pages#autor_site'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
