8cb95150f36b2927b6f517091d6223b31abd3826
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-31 00:04:54 +0400

    Add create_link_labels_hh
    Создадим метод для получения информации по каждой метке: количество записей

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,16 @@
 class HomeController < ApplicationController
   
+  def create_link_labels_hh
+    $labels_hh  = '48'
+  end
+
   def index
     if user_signed_in?
       $labels_cur = Label.where(user_id: current_user.id).order(:name)
-      $labels_hh  = '44' 
+      create_link_labels_hh 
     else
       $labels_cur = Label.all.order(:name)   # Здесь должно быть подругому...
+      create_link_labels_hh 
     end
   end
 
