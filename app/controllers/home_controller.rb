0d3cb6eff00b87c9dce46ee10276b493e0af2924
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-30 20:56:24 +0400

    Add sort_by name to $labels
    Добавление сортировки в результаты отбора названия меток из БД (при пользователе и при госте)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   
   def index
     if user_signed_in?
-      $labels_cur = Label.where(user_id: current_user.id)
+      $labels_cur = Label.where(user_id: current_user.id).order(:name)
     else
-      $labels_cur = Label.all   # Здесь должно быть подругому...
+      $labels_cur = Label.all.order(:name)   # Здесь должно быть подругому...
     end
   end
 
