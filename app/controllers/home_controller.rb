384bca419c8c1c35b2071fde510f298e22a7ff24
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-30 23:55:41 +0400

    Add stub for left-menu labels item
    Добавим заглушку для вывода количества записей по каждой метке.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       $labels_cur = Label.where(user_id: current_user.id).order(:name)
+      $labels_hh  = '44' 
     else
       $labels_cur = Label.all.order(:name)   # Здесь должно быть подругому...
     end
