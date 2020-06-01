94161005f857463dde148747654f77e1808e6400
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-06-01 19:39:42 +0400

    Changed the conditions for selecting data from db
    Изменено условие отбора данных из БД.
    Теперь будут выбираться только публичные запаси.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,7 +16,7 @@ class HomeController < ApplicationController
       $labels_cur = Label.where(user_id: current_user.id).order(:name)
       create_link_labels_hh 
     else
-      $notes_cur = Note.all
+      $notes_cur = Note.where(public: true)
       $labels_cur = Label.all.order(:name)   # Здесь должно быть подругому...
       create_link_labels_hh 
     end
