fb0ba592fd43e9e99ca5ceab397777d6e64aaa3b
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-06-01 19:27:11 +0400

    Add real amount button names in left menu
    Добавить реальное количество к названиям кнопок в левом меню

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,22 @@
 class HomeController < ApplicationController
   
   def create_link_labels_hh
-    $labels_hh  = '48'
+    $labels_hh  = {}
+    $labels_cur.each do |label|
+      $labels_hh[label.id] = 0
+    end
+    $notes_cur.each do |note|
+      $labels_hh[note.label_id] += 1
+    end
   end
 
   def index
     if user_signed_in?
+      $notes_cur = Note.where(user_id: current_user.id)
       $labels_cur = Label.where(user_id: current_user.id).order(:name)
       create_link_labels_hh 
     else
+      $notes_cur = Note.all
       $labels_cur = Label.all.order(:name)   # Здесь должно быть подругому...
       create_link_labels_hh 
     end
