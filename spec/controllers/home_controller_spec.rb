ed5f0810cff70b4ffc1209c4035c3885bcd0bca8
OPR-A-RUBin <opr.a.ruby@gmail.com>
2020-07-22 19:31:57 +0400

    Two test to home controller
    Не решён вопрос с состоянием пользователя....

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,21 @@ require "rails_helper"
 
 RSpec.describe HomeController, :type => :controller do
   describe "GET index" do
-    it "has a 200 status code" do
-      get :index
-      expect(response.status).to eq(200)
+  	
+  	let(:notes) {create_list :note, 3}
+
+  	before { get :index}
+
+  	context 'require output per pages' do
+  		
+  		it '__render to index template' do
+      	is_expected.to render_template :index 
+      end
+      
+      it '__instance var notes include only notes' do
+      	expect(assigns(key :notes)).to match_array(notes)		
+  		end	
+    
     end
   end
 end
