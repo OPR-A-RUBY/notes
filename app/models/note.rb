41791ad57024c5af42a10f58a2784e8dbad72d09
OPR-A-RUBin <opr.a.ruby@gmail.com>
2020-08-17 23:29:25 +0400

    has_and_belongs_to_many

--- app/models/note.rb
+++ app/models/note.rb
@@ -26,4 +26,8 @@ class Note < ApplicationRecord
 
   # Связь с таблицей молдели Label: (Note принадлежит одной :Label) 
   belongs_to :label
+
+  # Связь с таблицей Tags по принципу многие_ко_многим
+  has_and_belongs_to_many :tags
+  # Через вспомогательную таблицу Notes_tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ApplicationRecord
+
+	# Связь с таблицей Notes по принципу многие_ко_многим
+	has_and_belongs_to_many :notes 
+	# Через вспомогательную таблицу Notes_tags
+
 end
